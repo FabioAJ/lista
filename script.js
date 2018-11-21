7fe3f9013d73b119704de56dae06bc1fa5a0ca9a
FabioAJ <fabioaugusto.aj@gmail.com>
2018-11-21 17:58:22 -0200

    Lista Funcionando

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function verifica () {
-    /*if (event.which == 13 || event.keyCode == 13) {
-        return false;
-        console.log("1");
+function verifica(event) {
+    //id da verificação
+    var tarefa = document.getElementById("input").value;
+
+    if (event.which == 13 || event.keyCode == 13) {
+        if (tarefa == "") {
+            alert("Insira uma tarefa");
+        } else {
+            var barraTarefas = document.createElement("DIV");
+            var texto = document.createTextNode(tarefa);
+            barraTarefas.appendChild(texto);
+            document.body.appendChild(barraTarefas);
+        }
     }
-    return true;*/
-    console.log("2");
-};
+}
